c106ed3cca4a9bdb21980f89249501185a07dc37
Nicola <ciao@ciao>
2020-10-03 16:43:23 +0200

    Adds pascal's triangle

--- src/02/exercises.ml
+++ src/02/exercises.ml
@@ -13,4 +13,30 @@ let swap_adjacent s =
       )
   in step s 0 |> Buffer.contents
 
-
+(* Exercise 2 *)
+let print_pascal k =
+  let option_sum el o = match o with
+    | None -> el
+    | Some a -> a + el
+  in let rec pascal_row index acc =
+    if (index > k) then     
+      acc
+    else 
+      let (prev::tail) = acc
+      in let next = (List.mapi (fun index el -> (
+        List.nth_opt prev (index + 1)
+        |> option_sum el
+      )) prev)
+      in pascal_row (index + 1) ((1::next)::acc)
+  in pascal_row 2 [[1]] 
+  |> List.rev
+  |> List.iter (fun row -> (
+    List.iter (fun it -> print_int it; print_string " ") row;
+    print_endline "";
+  ))
+ 
+(* Exercise 3 *)
+(*
+let count_change d n =
+  let valid = List.filter 
+  *)    
